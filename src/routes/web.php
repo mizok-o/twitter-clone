044448d64a6aa7f6fb8471174cae71d58d83a786
溝口 耕太郎 <mizoguchikotaro@Hajimari-kotaromizoguchi.local>
2022-06-07 11:28:49 +0900

    認証機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
